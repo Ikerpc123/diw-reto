bc1feaf70ea6bf0036041be8e7068dd1d6d54d69
Ikerpc123 <ikerpc73@educastur.es>
2024-11-25 09:32:47 +0100

    Index base

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,33 @@
 let currentSlide = 0;
+let autoSlideInterval;
 
 function moveSlide(direction) {
   const slides = document.querySelectorAll('.carousel-item');
   const totalSlides = slides.length;
 
-  // Ocultar la diapositiva actual
   slides[currentSlide].classList.remove('active');
-
-  // Calcular el nuevo índice
   currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
-
-  // Mostrar la nueva diapositiva
   slides[currentSlide].classList.add('active');
 
-  // Mover el contenedor del carrusel
   const carouselInner = document.querySelector('.carousel-inner');
   carouselInner.style.transform = `translateX(-${currentSlide * 100}%)`;
 }
+
+function startAutoSlide() {
+  autoSlideInterval = setInterval(() => {
+    moveSlide(1);
+  }, 3000);
+}
+
+function stopAutoSlide() {
+  clearInterval(autoSlideInterval);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  startAutoSlide();
+
+  const controls = document.querySelectorAll('.carousel-control');
+  controls.forEach(control => {
+    control.addEventListener('click', stopAutoSlide);
+  });
+});
